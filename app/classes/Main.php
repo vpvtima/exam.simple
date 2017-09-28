0056d955bf6d327bc082885816a8cda91c145c3b
Vasin Semen <vsp.tima@gmail.com>
2017-09-28 19:31:56 +0300

    Созданы классы и главный файл индекс разобран на другие файлы.

--- app/classes/Main.php
+++ app/classes/Main.php
@@ -1,2 +1,20 @@
 <?php
 // аналог класса Page
+require_once 'Seo.php';
+class Page extends  Seo
+{
+    public function _construct()
+    {}
+    public function header()
+    {
+
+    }
+    public function footer()
+    {
+
+    }
+    public function loadData()
+    {
+
+    }
+}
--- app/classes/Seo.php
+++ app/classes/Seo.php
@@ -1,2 +1,10 @@
 <?php
 // аналог класса Seo
+class Seo
+{
+    public $title;
+    public $description;
+    public $author="Vasin S.P.";
+    public $copyright="@ exam.simple.vspds.ru 2017";
+    public $projectName = "Another simple PHP project";
+}
