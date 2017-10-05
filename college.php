22429d7dd03eff91c0e60a3ec5ad7af5bce043f7
Vasin Semen <vsp.tima@gmail.com>
2017-10-05 19:37:52 +0300

    Работа выполнена

--- college.php
+++ college.php
@@ -4,13 +4,23 @@ $main->title = 'Колледж';
 $main->description = 'Описание колледжа';
 $main->header();
 ?>
-    <h1><?php echo $main->title; ?></h1>
-    <p class="lead"><?php echo $main->description; ?></p>
-    <?php
-    $xml = simplexml_load_file('app/data/college.xml');
-    ?>
-    <?php foreach ($xml as $val):?>
-        <?= (string)$val->title;?>
-        <?= (string)$val->content; ?>
-    <?php endforeach; ?>
+
+    <div class="jumbotron">
+        <h1><?php echo $main->title; ?></h1>
+        <p class="lead"><?php echo $main->description; ?></p>
+        <small>данные берутся из <span class="badge">xml</span> файла</small>
+    </div>
+    <div class="row marketing">
+        <h1><?php echo $main->title; ?></h1>
+        <p class="lead"><?php echo $main->description; ?></p>
+        <?php
+        $xml = simplexml_load_file('app/data/college.xml');
+        ?>
+        <?php foreach ($xml as $val):?>
+            <div class="panel panel-default">
+                <div class="panel-heading"><?= (string)$val->title;?></div>
+                <div class="panel-body"><?= (string)$val->content; ?></div>
+            </div>
+        <?php endforeach; ?>
+    </div>
 <?php $main->footer(); ?>
--- index.php
+++ index.php
@@ -5,35 +5,83 @@ $main->description = 'Главная страница';
 $main->header();
 ?>
 
-    <!-- Jumbotron -->
     <div class="jumbotron">
-        <h1>Marketing stuff!</h1>
-        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet.</p>
-        <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p>
+        <h1>Hello, bro!</h1>
+        <p class="lead">Обкатка простейших web-технологий</p>
+        <p><small>С использованием PHP, PhpStorm, Github, Git, JSON, XML, CSV, Html, Bootstrap 3, Data, Apache</small></p>
+        <p><a target="_blank" class="btn btn-lg btn-success" href="https://github.com/vsptima/simple" role="button">Go to Github!</a></p>
     </div>
 
-    <!-- Example row of columns -->
     <div class="row">
-        <div class="col-lg-4">
-            <h2>Safari bug warning!</h2>
-            <p class="text-danger">As of v9.1.2, Safari exhibits a bug in which resizing your browser horizontally causes rendering errors in the justified nav that are cleared upon refreshing.</p>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <h3>PHP</h3>
+                </div>
+            </div>
         </div>
-        <div class="col-lg-4">
-            <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <h3>PhpStorm</h3>
+                </div>
+            </div>
         </div>
-        <div class="col-lg-4">
-            <h2>Heading</h2>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-            <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <h3>Github</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <h3>JSON</h3>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <h3>XML</h3>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <h3>CSV</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <h3>Bootstrap 3</h3>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <h3>Data</h3>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <h3>Apache</h3>
+                </div>
+            </div>
         </div>
     </div>
 
-
-    <!-- Site footer -->
 <?php
 $main->footer();
 ?>
